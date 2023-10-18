cc5eac1961e259adb144892eb47799795a724cd4
Student <student@LT-27501.khk.local>
2023-10-18 16:30:25 +0300

    Created product details screen

--- src/screens/ProductDetails/styles.js
+++ src/screens/ProductDetails/styles.js
@@ -20,9 +20,43 @@ export const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: '500'
     },
+    price: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginVertical: 8
+    },
     description: {
         color: colors.textGrey,
         fontWeight: '300',
         marginVertical: 8
+    },
+    footer: {
+        padding: 24,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    save: {
+        flex: 1
+    },
+    bookmarkContainer: {
+        backgroundColor: colors.lightGrey,
+        padding: 18,
+        borderRadius: 8,
+        marginRight: 16
+    },
+    bookmarkIcon: {
+        width: 24,
+        height: 24
+    },
+    backContainer: {
+        backgroundColor: colors.white,
+        padding: 10,
+        margin: 24,
+        borderRadius: 8,
+        position: 'absolute'
+    },
+    backIcon: {
+        width: 20,
+        height: 20
     }
 })
