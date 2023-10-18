31920709c7c3f14a1084228b4d14a8b7aa335913
Student <student@LT-27501.khk.local>
2023-10-18 17:50:57 +0300

    started creating profile and settings screens

---
+++ src/screens/app/Settings/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../../utils/colors";
+
+export const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        flex: 1
+    },
+    item: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        marginVertical: 8
+    },
+    sectionTitle: {
+        fontWeight: '500',
+        fontSize: 16,
+        color: colors.grey,
+        marginBottom: 16
+    }
+})
